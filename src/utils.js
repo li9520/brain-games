ac3c159c38a575b01b086376f226f2e4031ec9a0
li9520 <li.dp9520@gmail.com>
2022-12-03 11:53:51 +0500

    Добавление getRandomIndex()

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/prefer-default-export
 const getRandomValue = (max) => Math.floor(Math.random() * max);
+const getRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
 const isEven = (value) => (value % 2 === 0);
 
-export { getRandomValue, isEven };
+export { getRandomValue, getRandomIndex, isEven };
